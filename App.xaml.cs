c6d2a825e510759c49a06c2f1836b8c6d4495297
rkoolyk <rachelkoolyk@gmail.com>
2021-04-01 18:39:17 +0300

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ED1FlightSimulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        public int BoundTop { get; set; }
+
+        private void ArrowDown_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("!!!!");
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void onPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
