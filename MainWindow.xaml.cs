c968972501831bc7c0a10091711e0938a58b0a3c
ronigot <ronigotlib@gmail>
2021-04-02 10:50:17 +0300

    Adding variables for visibility of positions on joystick

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,15 @@ namespace ED1FlightSimulator
             InitializeComponent();
             ThrottleSlider.SetValue(Canvas.TopProperty, BoundTop);
             RudderSlider.SetValue(Canvas.LeftProperty, BoundLeft);
+            CenterPos.SetValue(VisibilityProperty, Center);
+            UpPos.SetValue(VisibilityProperty, Up);
+            DownPos.SetValue(VisibilityProperty, Down);
+            LeftPos.SetValue(VisibilityProperty, Left);
+            RightPos.SetValue(VisibilityProperty, Right);
+            UpRightPos.SetValue(VisibilityProperty, UpRight);
+            UpLeftPos.SetValue(VisibilityProperty, UpLeft);
+            DownLeftPos.SetValue(VisibilityProperty, DownLeft);
+            DownRightPos.SetValue(VisibilityProperty, DownRight);
         }
 
         private double _boundTop = 150;
@@ -59,6 +68,172 @@ namespace ED1FlightSimulator
                 onPropertyChanged("BoundLeft");
             }
         }
+
+        private Visibility _center = Visibility.Visible;
+
+        public Visibility Center
+        {
+            get
+            {
+                return _center;
+            }
+            set
+            {
+                if (_center != value)
+                {
+                    _center = value;
+                }
+                onPropertyChanged("Center");
+            }
+        }
+        
+        
+        private Visibility _up = Visibility.Hidden;
+
+        public Visibility Up
+        {
+            get
+            {
+                return _up;
+            }
+            set
+            {
+                if (_up != value)
+                {
+                    _up = value;
+                }
+                onPropertyChanged("Up");
+            }
+        }
+        
+        private Visibility _down = Visibility.Hidden;
+
+        public Visibility Down
+        {
+            get
+            {
+                return _down;
+            }
+            set
+            {
+                if (_down != value)
+                {
+                    _down = value;
+                }
+                onPropertyChanged("Down");
+            }
+        }
+        
+        private Visibility _left = Visibility.Hidden;
+
+        public Visibility Left
+        {
+            get
+            {
+                return _left;
+            }
+            set
+            {
+                if (_left != value)
+                {
+                    _left = value;
+                }
+                onPropertyChanged("Left");
+            }
+        }
+        
+        private Visibility _right = Visibility.Hidden;
+
+        public Visibility Right
+        {
+            get
+            {
+                return _right;
+            }
+            set
+            {
+                if (_right != value)
+                {
+                    _right = value;
+                }
+                onPropertyChanged("Right");
+            }
+        }
+        
+        private Visibility _upRight = Visibility.Hidden;
+
+        public Visibility UpRight
+        {
+            get
+            {
+                return _upRight;
+            }
+            set
+            {
+                if (_upRight != value)
+                {
+                    _upRight = value;
+                }
+                onPropertyChanged("UpRight");
+            }
+        }
+        
+        private Visibility _downRight = Visibility.Hidden;
+
+        public Visibility DownRight
+        {
+            get
+            {
+                return _downRight;
+            }
+            set
+            {
+                if (_downRight != value)
+                {
+                    _downRight = value;
+                }
+                onPropertyChanged("DownRight");
+            }
+        }
+        
+        
+        private Visibility _downLeft = Visibility.Hidden;
+
+        public Visibility DownLeft
+        {
+            get
+            {
+                return _downLeft;
+            }
+            set
+            {
+                if (_downLeft != value)
+                {
+                    _downLeft = value;
+                }
+                onPropertyChanged("DownLeft");
+            }
+        }
+
+        private Visibility _upLeft = Visibility.Hidden;
+
+        public Visibility UpLeft
+        {
+            get
+            {
+                return _upLeft;
+            }
+            set
+            {
+                if (_upLeft != value)
+                {
+                    _upLeft = value;
+                }
+                onPropertyChanged("UpLeft");
+            }
+        }
+        
+
 
 
         private void ArrowDown_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
