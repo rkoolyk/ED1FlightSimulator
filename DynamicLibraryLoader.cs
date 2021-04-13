e0e4af2fa1f786c01ba7df2ca7d40c829c14d392
tashmanr <rebeccatashman@gmail.com>
2021-04-13 14:51:35 +0300

    bringing GetRelevantTimesteps to DynamicLibraryLoader.cs

--- DynamicLibraryLoader.cs
+++ DynamicLibraryLoader.cs
@@ -40,13 +40,14 @@ namespace ED1FlightSimulator
         private IntPtr TimeSeries;
         private List<string> dataList;
         private Dictionary<String, String> correlations = new Dictionary<String, String>();
+        private Dictionary<String, List<float>> relevantTimeSteps = new Dictionary<string, List<float>>();
         private String csvPath;
         private String regFlightPath;
-        private String xmlPath;
         IntPtr algoDLL;
         IntPtr pCreateSAD;
         IntPtr pMostCorrelatedFeature;
         IntPtr pFindLinReg;
+        IntPtr pGetTimeSteps;
         IntPtr timeSeriesDLL;
         IntPtr pCreate;
         IntPtr pgetRowSize;
@@ -60,6 +61,7 @@ namespace ED1FlightSimulator
             pCreateSAD = NativeMethods.GetProcAddress(algoDLL, "CreateSAD");
             pMostCorrelatedFeature = NativeMethods.GetProcAddress(algoDLL, "MostCorrelatedFeature");
             pFindLinReg = NativeMethods.GetProcAddress(algoDLL, "findLinReg");
+            pGetTimeSteps = NativeMethods.GetProcAddress(algoDLL, "getTimeSteps");
             CreateSAD CreateSAD = (CreateSAD)Marshal.GetDelegateForFunctionPointer(pCreateSAD, typeof(CreateSAD));
             AnomalyDetector = CreateSAD();
             Thread thread = new Thread(
@@ -68,23 +70,24 @@ namespace ED1FlightSimulator
                     CreateCorrelations();
                 });
             thread.Start();
-            //CreateCorrelations();
             return AnomalyDetector;
         }
 
-        public  void LineReg(ref float a, ref float b, String category, String correlatedCategory)
+        public void LineReg(ref float a, ref float b, String category, String correlatedCategory)
         {
-            findLinReg findLinReg =(findLinReg)Marshal.GetDelegateForFunctionPointer(pFindLinReg, typeof( findLinReg));
+            findLinReg findLinReg = (findLinReg)Marshal.GetDelegateForFunctionPointer(pFindLinReg, typeof(findLinReg));
             findLinReg(TimeSeries, ref a, ref b, category, correlatedCategory);
         }
         public void CreateCorrelations()
         {
             MostCorrelatedFeature MostCorrelatedFeature = (MostCorrelatedFeature)Marshal.GetDelegateForFunctionPointer(pMostCorrelatedFeature, typeof(MostCorrelatedFeature));
-            StringBuilder tmp = new StringBuilder();   
-            foreach(String category in dataList)
+            StringBuilder tmp = new StringBuilder();
+            foreach (String category in dataList)
             {
                 MostCorrelatedFeature(AnomalyDetector, regFlightPath, dataList.ToArray(), dataList.Count(), category, tmp);
-                correlations.Add(category, tmp.ToString());
+                String tmpString = tmp.ToString();
+                correlations.Add(category, tmpString);
+                //relevantTimeSteps.Add(category, GetAllTimestepsForeAnomalies(category, tmpString));
             }
         }
 
@@ -103,8 +106,9 @@ namespace ED1FlightSimulator
             }
         }
 
-        public  IntPtr CreateTS(String timeSeriesPath, String csvPath, List<string> data)
+        public IntPtr CreateTS(String timeSeriesPath, String csvPath, List<string> data)
         {
+            this.csvPath = csvPath;
             timeSeriesDLL = NativeMethods.LoadLibrary(@timeSeriesPath);
             pCreate = NativeMethods.GetProcAddress(timeSeriesDLL, "Create");
             pgetRowSize = NativeMethods.GetProcAddress(timeSeriesDLL, "getRowSize");
@@ -131,8 +135,42 @@ namespace ED1FlightSimulator
             }
             return tsDic;
         }
-    }
+
+        public List<float> GetAllTimestepsForeAnomalies(String category, String correlatedCategory)
+        {
+            getTimeSteps getTimeSteps = (getTimeSteps)Marshal.GetDelegateForFunctionPointer(pGetTimeSteps, typeof(getTimeSteps));
+            List<float> TimeStepList = new List<float>();
+            String oneWay = category + "-" + correlatedCategory;
+            String otherWay = correlatedCategory + "-" + category;
+            StringBuilder arr = new StringBuilder(512);
+            int lenOfSattslist = dataList.Count();
+            getTimeSteps(AnomalyDetector, csvPath, dataList.ToArray(), lenOfSattslist, oneWay, otherWay, arr);
+            String temper = arr.ToString();
+            if (String.Equals(temper, "no timesteps"))
+            {
+                return TimeStepList;
+            }
+
+            string[] words = temper.Split(' ');
+
+            for (int i = 0; i < words.Count(); i++)
+            {
+                float temp = float.Parse(words[i]);
+                TimeStepList.Add(temp);
+            }
+            return TimeStepList;
+        }
+
+        public List<float> GetRelevantTimesteps(String category, String correlatedCategory)
+        {
+            if (relevantTimeSteps.ContainsKey(category))
+            {
+                return relevantTimeSteps[category];
+            }
+            return new List<float>();  //GetAllTimestepsForeAnomalies(category, correlatedCategory);
+        }
 
 
     }
+}
 
